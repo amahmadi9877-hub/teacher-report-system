bcf7efb58459aeb8f0f298d0d30f5d87ad8af0dc
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-04 11:11:31 +0330

    Create User model

---
+++ accounts/models/user.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from accounts.enums import UserRole
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=14, blank=True, null=True)
+    backup_phone_number = models.CharField(max_length=14, blank=True, null=True)
+    role = models.CharField(
+        max_length=20,
+        choices=UserRole.choices,
+    )
+    REQUIRED_FIELDS = ["first_name", "last_name", "role", "phone_number"]
+
+    def __str__(self):
+        return self.get_full_name()
