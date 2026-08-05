a03b41e5f6267fe081d98d419526a4d4ee367f9a
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-05 13:16:40 +0330

    Define __init__.py for courses models

---
+++ courses/models/__init__.py
@@ -0,0 +1,13 @@
+from courses.models.academic_term import AcademicTerm
+from courses.models.course_instructor import CourseInstructor
+from courses.models.course_schedule import CourseSchedule
+from courses.models.course_session import CourseSession
+from courses.models.course import Course
+
+__all__ = [
+    "AcademicTerm",
+    "CourseInstructor",
+    "CourseSchedule",
+    "CourseSession",
+    "Course",
+]
