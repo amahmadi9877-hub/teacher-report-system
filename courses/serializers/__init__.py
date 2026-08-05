dc246beaf1244c9b88ad1cc8c2e672fd100ef053
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-05 22:07:03 +0330

    Define __init__.py for serializers of courses

---
+++ courses/serializers/__init__.py
@@ -0,0 +1,7 @@
+from courses.serializers.academic_term_serializer import AcademicTermModelSerializer
+from courses.serializers.courser_serializer import CourseModelSerializer
+
+__all__ = [
+    "AcademicTermModelSerializer",
+    "CourseModelSerializer",
+]
