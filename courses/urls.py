950252a99531f5994c545b2b5ec07b844643532f
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-05 22:07:26 +0330

    Define courses urls

---
+++ courses/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from courses.views import AcademicTermAPIModelViewSet, CourseAPIModelViewSet
+
+router = DefaultRouter(use_regex_path=False)
+router.register(
+    prefix="academic-term",
+    viewset=AcademicTermAPIModelViewSet,
+    basename="academic_term",
+)
+router.register(prefix="course", viewset=CourseAPIModelViewSet, basename="course")
+
+urlpatterns = []
+
+urlpatterns += router.urls
