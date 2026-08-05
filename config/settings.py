586f88fba2c2f7c6efcc14647e956343ea973d2b
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-05 22:07:58 +0330

    Add EXCEPTION_HANDLER to the setting

--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "core.exceptions.custom_exception_handler",
+}
