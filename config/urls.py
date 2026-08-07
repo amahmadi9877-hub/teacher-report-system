0b6a0314fb43020b56582cfcd93495f50bbebcec
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-07 12:18:09 +0330

    Add jwt related paths

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("organizations/", include("organizations.urls"), name="organization_api"),
     path("course/", include("courses.urls"), name="course_api"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
