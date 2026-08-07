60192cc8b1c549e33f86425ebf64bbd7ef5a1d1a
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-07 12:20:11 +0330

    Update create user mangement command

--- accounts/management/commands/create_user.py
+++ accounts/management/commands/create_user.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 from django.db import DataError, IntegrityError
-
 from accounts.enums import UserRole
 from accounts.models.user import User
 from core.utils import required_input, get_password
+
+AUTH_USER = get_user_model()
 
 
 class Command(BaseCommand):
@@ -12,30 +14,35 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "role",
+            "--role",
             choices=[value for value in UserRole.selectable_choices()],
+            required=True,
         )
+        parser.add_argument("--username", required=True)
+        parser.add_argument("--first-name", required=True)
+        parser.add_argument("--last-name", required=True)
+        parser.add_argument("--phone-number", required=True)
+        parser.add_argument("--backup-phone-number", required=False)
+        parser.add_argument("--password", required=True)
 
     def handle(self, *args, **options):
-        username = required_input("Username: ")
-        first_name = required_input("Firstname: ")
-        last_name = required_input("Lastname: ")
-        phone_number = required_input("PhoneNumber: ")
-        backup_phone_number = (
-            required_input("Backup PhoneNumber: ")
-            if options["role"] == UserRole.TEACHER
-            else input("Backup PhoneNumber: ")
+        if (
+            not options.get("backup_phone_number")
+            and options.get("role") == UserRole.TEACHER
+        ):
+            raise CommandError(
+                "backup-phone-number is required for teachers!(--backup-phone-number)"
+            )
+
+        user = AUTH_USER(
+            username=options.get("username"),
+            first_name=options.get("first_name"),
+            last_name=options.get("last_name"),
+            role=options.get("role"),
+            phone_number=options.get("phone_number"),
+            backup_phone_number=options.get("backup_phone_number"),
         )
-        password = get_password("Password: ")
-        user = User(
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            role=options["role"],
-            phone_number=phone_number,
-            backup_phone_number=backup_phone_number,
-        )
-        user.set_password(password)
+        user.set_password(options.get("password"))
         try:
             user.save()
         except IntegrityError as i:
