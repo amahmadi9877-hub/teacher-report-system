24d52bd3f66d528b86a654acd7d42ca4536ee817
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-07 12:07:46 +0330

    Modify UserRole enum

--- accounts/enums.py
+++ accounts/enums.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class UserRole(models.TextChoices):
-    ADMIN = "admin"
-    TEACHER = "teacher"
-    EDUCATION_OFFICER = "education_officer"
-    FINANCE_OFFICER = "finance_officer"
+    ADMIN = "admin", "Admin"
+    TEACHER = "teacher", "Teacher"
+    EDUCATION_OFFICER = "education_officer", "Education_Officer"
+    FINANCE_OFFICER = "finance_officer", "Finance_Officer"
 
     @classmethod
     def selectable_choices(cls):
