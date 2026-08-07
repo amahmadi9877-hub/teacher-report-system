f1e1d97b2a60931b92286708916e5d05726d578f
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-07 12:09:47 +0330

    Apply Education Officer or admin and Authenticated permissions to views

--- courses/views/academic_term.py
+++ courses/views/academic_term.py
@@ -1,9 +1,12 @@
+from rest_framework.permissions import IsAuthenticated
+from core.permissions import IsEducationOfficerOrAdmin
 from core.views import BaseModelViewSet
 from courses.models import AcademicTerm
 from courses.serializers import AcademicTermModelSerializer
 
 
 class AcademicTermAPIModelViewSet(BaseModelViewSet):
+    permission_classes = [IsEducationOfficerOrAdmin, IsAuthenticated]
     model = AcademicTerm
     queryset = AcademicTerm.objects.all()
     serializer_class = AcademicTermModelSerializer
