baab835f9517298d9c6e1a3da5d5eb2739330c9d
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-12 11:34:29 +0330

    Update __init__.py for serializers of core

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,10 +2,14 @@ from core.serializers.base_model_serializer import BaseModelSerializer
 from core.serializers.error_serializer import ErrorSerializer
 from core.serializers.assign_serializer import AssignSerializer
 from core.serializers.set_owner_serializer import SetOwnerSerializer
+from core.serializers.activate_serializer import ActivateSerializer
+from core.serializers.deactivate_serializer import DeactivateSerializer
 
 __all__ = [
     "BaseModelSerializer",
     "ErrorSerializer",
     "AssignSerializer",
     "SetOwnerSerializer",
+    "ActivateSerializer",
+    "DeactivateSerializer",
 ]
