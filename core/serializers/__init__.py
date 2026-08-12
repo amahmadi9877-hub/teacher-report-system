aefd80155b64dfb1a27b0ce97e485e702365ab4e
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-12 10:15:12 +0330

    Add base set_owner serializer

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,5 +1,11 @@
 from core.serializers.base_model_serializer import BaseModelSerializer
 from core.serializers.error_serializer import ErrorSerializer
 from core.serializers.assign_serializer import AssignSerializer
+from core.serializers.set_owner_serializer import SetOwnerSerializer
 
-__all__ = ["BaseModelSerializer", "ErrorSerializer", "AssignSerializer"]
+__all__ = [
+    "BaseModelSerializer",
+    "ErrorSerializer",
+    "AssignSerializer",
+    "SetOwnerSerializer",
+]
