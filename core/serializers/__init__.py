afacefa758d55ebe006e53b6a6c316251617d66f
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-12 09:27:11 +0330

    Modify Assign Serializer

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,7 +1,5 @@
 from core.serializers.base_model_serializer import BaseModelSerializer
 from core.serializers.error_serializer import ErrorSerializer
+from core.serializers.assign_serializer import AssignSerializer
 
-__all__ = [
-    "BaseModelSerializer",
-    "ErrorSerializer",
-]
+__all__ = ["BaseModelSerializer", "ErrorSerializer", "AssignSerializer"]
