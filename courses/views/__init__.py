9f18ceffd46cc6306e4f5b29491552d6e4032f4c
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-14 13:30:19 +0330

    Update __init__.py of views for courses

--- courses/views/__init__.py
+++ courses/views/__init__.py
@@ -1,7 +1,13 @@
 from courses.views.academic_term import AcademicTermAPIModelViewSet
+from courses.views.course_instructor import CourseInstructorAPIModelViewSet
+from courses.views.course_schedule import CourseScheduleAPIModelViewSet
+from courses.views.course_session import CourseSessionAPIModelViewSet
 from courses.views.course import CourseAPIModelViewSet
 
 __all__ = [
     "AcademicTermAPIModelViewSet",
+    "CourseInstructorAPIModelViewSet",
+    "CourseScheduleAPIModelViewSet",
+    "CourseSessionAPIModelViewSet",
     "CourseAPIModelViewSet",
 ]
