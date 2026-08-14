254b5fe206ac5975a2b33a1692508f01a98e2581
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-14 13:13:09 +0330

    Reset weekday values

--- core/enums/week_day.py
+++ core/enums/week_day.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class WeekDay(models.IntegerChoices):
-    MONDAY = 1, "Mon"
-    TUESDAY = 2, "Tue"
-    WEDNESDAY = 3, "Wed"
-    THURSDAY = 4, "Thu"
-    FRIDAY = 5, "Fri"
-    SATURDAY = 6, "Sat"
-    SUNDAY = 7, "Sun"
+    MONDAY = 0, "Mon"
+    TUESDAY = 1, "Tue"
+    WEDNESDAY = 2, "Wed"
+    THURSDAY = 3, "Thu"
+    FRIDAY = 4, "Fri"
+    SATURDAY = 5, "Sat"
+    SUNDAY = 6, "Sun"
