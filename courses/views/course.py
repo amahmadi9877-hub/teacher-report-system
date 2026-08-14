c64fcfa10dd3e254c42573343610d1be82ebb02b
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-14 13:28:44 +0330

    Modify CourseAPIModelViewSet

--- courses/views/course.py
+++ courses/views/course.py
@@ -1,7 +1,12 @@
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from core.permissions import IsEducationOfficer, IsAdmin
 from core.views import BaseModelViewSet
 from courses.models import Course
-from courses.serializers import CourseModelSerializer
+from courses.serializers import CourseModelSerializer, CourseScheduleModelSerializer
+from courses.services import CourseScheduleService
 
 
 class CourseAPIModelViewSet(BaseModelViewSet):
@@ -10,3 +15,14 @@ class CourseAPIModelViewSet(BaseModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseModelSerializer
     lookup_url_kwarg = "pk"
+
+    @action(detail=True, methods=["POST"], url_path="create-schedule")
+    def create_course_schedule(self, request, pk):
+        course = self.get_object()
+
+        schedule = CourseScheduleService.create_from_course(course, request.user)
+        serializer = CourseScheduleModelSerializer(schedule)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+        )
