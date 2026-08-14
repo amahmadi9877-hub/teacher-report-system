803b28886531a1ab8968820ea46fe5463e8c1b61
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-14 13:18:22 +0330

    Add course-instructor and course-schedule and course-session to courses url

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from courses.views import AcademicTermAPIModelViewSet, CourseAPIModelViewSet
+from courses.views import (
+    AcademicTermAPIModelViewSet,
+    CourseAPIModelViewSet,
+    CourseInstructorAPIModelViewSet,
+    CourseScheduleAPIModelViewSet,
+    CourseSessionAPIModelViewSet,
+)
 
 router = DefaultRouter(use_regex_path=False)
 router.register(
@@ -10,6 +16,21 @@ router.register(
     basename="academic-term",
 )
 router.register(prefix="course", viewset=CourseAPIModelViewSet, basename="course")
+router.register(
+    prefix="course-instructor",
+    viewset=CourseInstructorAPIModelViewSet,
+    basename="course-instructor",
+)
+router.register(
+    prefix="course-schedule",
+    viewset=CourseScheduleAPIModelViewSet,
+    basename="course-schedule",
+)
+router.register(
+    prefix="course-session",
+    viewset=CourseSessionAPIModelViewSet,
+    basename="course-session",
+)
 
 urlpatterns = []
 
