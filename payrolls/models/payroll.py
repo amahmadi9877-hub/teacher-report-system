4e45357289fb3551cfd3bab59244424dd863d771
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-27 03:07:47 +0330

    update payroll model

--- payrolls/models/payroll.py
+++ payrolls/models/payroll.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from core.enums import Month
 from core.models import BaseModel
 from payrolls.enums import PayrollStatus
 
@@ -7,17 +8,23 @@ from payrolls.enums import PayrollStatus
 # Create your models here.
 class Payroll(BaseModel):
     STATUS_CHOICES = PayrollStatus
+    DEFAULT_ACTIVE_STATUS = {PayrollStatus.DRAFT}
     ACTIVE_STATUSES = {PayrollStatus.DRAFT}
-    INACTIVE_STATUSES = {PayrollStatus.CALCULATED, PayrollStatus.BANED}
-    DELETED_ACTIVE_STATUSES = {PayrollStatus.DELETED}
+    INACTIVE_STATUSES = {
+        PayrollStatus.CALCULATED,
+        PayrollStatus.BANED,
+        PayrollStatus.NO_SESSION,
+    }
+    DELETED_STATUSES = {PayrollStatus.DELETED}
     name = models.CharField(max_length=250)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    year = models.IntegerField()
+    month = models.IntegerField(choices=Month.choices)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     status = models.IntegerField(
         choices=list(PayrollStatus.choices),
         default=PayrollStatus.DRAFT,
     )
+    responsible_user = None
 
     def __str__(self):
         return self.name
