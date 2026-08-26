6ed8ddd504b74c2bac42d3c369656843a4caa79d
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-27 02:55:14 +0330

    Modify low  detail and high detail serializers for user

--- accounts/serializers/user_model_serializer.py
+++ accounts/serializers/user_model_serializer.py
@@ -3,7 +3,23 @@ from rest_framework import serializers
 from accounts.models import User
 
 
-class UserModelSerializer(serializers.ModelSerializer):
+class LowDetailUserModelSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "phone_number", "backup_phone_number"]
+        fields = ["id", "full_name"]
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip()
+
+
+class HighDetailUserModelSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ["id", "full_name", "phone_number", "backup_phone_number"]
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip()
