e1cd176683e4fef32586d02728083b2a3581545e
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-08-27 02:59:35 +0330

    update core base model serializer

--- core/serializers/base_model_serializer.py
+++ core/serializers/base_model_serializer.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers
-from accounts.serializers import UserModelSerializer
+from accounts.serializers import (
+    LowDetailUserModelSerializer,
+    HighDetailUserModelSerializer,
+)
 
 
 class BaseModelSerializer(serializers.ModelSerializer):
-    created_by = UserModelSerializer(read_only=True)
-    updated_by = UserModelSerializer(read_only=True)
+    created_by = LowDetailUserModelSerializer(read_only=True)
+    updated_by = LowDetailUserModelSerializer(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
     state = serializers.IntegerField(read_only=True)
     status = serializers.IntegerField(read_only=True)
-    owner_user = UserModelSerializer(read_only=True)
-    responsible_user = UserModelSerializer(read_only=True)
+    owner_user = HighDetailUserModelSerializer(read_only=True)
+    responsible_user = LowDetailUserModelSerializer(read_only=True)
 
     def validate(self, attrs):
 
